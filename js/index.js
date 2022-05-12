42f391c78c922d0bc5258052bd62de40a39f7b74
El-luca <lucas.lnss1994@gmail.com>
2022-05-11 22:25:43 -0300

    add button +/-

--- js/index.js
+++ js/index.js
@@ -2,9 +2,11 @@ const $buttonsNumbers = document.querySelectorAll('.calculator__buttons-nunber')
 const $inputCalculate = document.querySelector('.calculate')
 const $buttonsOperator = document.querySelectorAll('.calculator__buttons-operators')
 const $divison = document.querySelector('.calculator__buttons-operator-division')
+const $multiplication = document.querySelector('.multiplication')
 const $result = document.querySelector('.calculator__display-result')
 const $equal =  document.querySelector('.calculator__buttons-equal')
 const $clean = document.querySelector('.clean')
+const $negativePositive = document.querySelector('.button-negative-or-positive')
 
 let calculate = ''
 const clean = ''
@@ -23,14 +25,18 @@ $buttonsOperator.forEach(function($button){
         $inputCalculate.value = calculate.replaceAll(' ', '')
     })
 })
-console.log($divison)
+
 $divison.addEventListener('click', function(){
     calculate = calculate + '/'
     $inputCalculate.value = calculate
 })
 
+$multiplication.addEventListener('click', function(){
+    calculate = calculate + '*'
+    $inputCalculate.value = calculate
+})
+
 $equal.addEventListener('click', function(){
-    console.log(calculate)
     if(calculate !== ''){
         $result.textContent = eval(calculate)
     }else{
@@ -38,6 +44,14 @@ $equal.addEventListener('click', function(){
     }
 })
 
+$negativePositive.addEventListener('click', function(){
+    $inputCalculate.value = $inputCalculate.value != '-' ? '-' +  $inputCalculate.value : $inputCalculate.value
+    console.log($inputCalculate.value)
+})
+
 $clean.addEventListener('click', function(){
+    $inputCalculate.value = ''
+    $result.innerHTML = ''
+    $equal.textContent = ''
     document.location.reload()
 })
